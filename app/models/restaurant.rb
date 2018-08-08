59faaf8f4fa0ae0411482a1ec5fe46daa7e4744e
Sonya Doyle <sonyadoyle@me.com>
2018-08-08 12:06:19 +1000

    applied validation rules for Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
 end
+
+
+# restaurant must have at least:
+# a name, an address and a category.
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 end
+
+
+# A review must have a parent restaurant.
+# A review must have content and a rating.
+# The rating should be a number between 0 and 5.
