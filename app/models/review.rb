a0477ae75c9099109924fe14e2eb570860c1b61a
Sonya Doyle <sonyadoyle@me.com>
2018-08-08 12:15:30 +1000

    added validation rules for Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 end
 
 
 # A review must have a parent restaurant.
 # A review must have content and a rating.
-# The rating should be a number between 0 and 5.
+# The rating should be a number between 0 and 5
+
