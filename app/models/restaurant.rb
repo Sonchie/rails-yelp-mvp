d5ead8feb976b466f2dbaea89762526f389f30e5
Sonya Doyle <sonyadoyle@me.com>
2018-08-08 12:19:27 +1000

    updated additional ruled for Restaurant Model - category

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, :address, :category, presence: true
+  validates :name, :address, presence: true
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
 
 
 # restaurant must have at least:
 # a name, an address and a category.
+# restaurant category should belong to fixed list [
+# "chinese", "italian", "japanese", "french", "belgian"]
